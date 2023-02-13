a87126bc873b12385fa307655c3af46b789d64bc
Mr.Jor <62343478+in-depth-1@users.noreply.github.com>
2023-02-13 10:21:48 +0800

    更新了 git.zsh 配置文件

--- config/git.zsh
+++ config/git.zsh
@@ -1,3 +1,6 @@
+ga() { git add "$*" }
+gm() { git commit -m "$*" }
+gam() { git add --all && git commit -m "$*" }
 gll() {
   git log --pretty=format:'%C(#98c379)%h %C(#26c6da)%ad%C(#ff5252): %C(#b0bec5)%s%C(#ef6c00)%d %C(#3eb370)<%an>' --graph --date=short
 }
